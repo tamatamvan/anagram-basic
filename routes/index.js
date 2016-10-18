6d1669636e3e428c17482ddf93911ff6b1752960
Alexander Hendrawan tjahja & Juang Wiantoro <pair+alexanderHT.broerjuang@devbootcamp.com>
2016-10-18 15:14:46 +0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var helper = require('../helpers/util');
+var fs  = require('fs');
 
 /* GET home page. */
 
---
+++ seeders/20161018075955-entry_words.js
@@ -0,0 +1,31 @@
+'use strict';
+let file = '../db/fixtures/words.txt';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    let data_array = fs.readFileSync(....)
+
+    return queryInterface.bulkInsert('Person', data_array, {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
