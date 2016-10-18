bdc9cbbf40ca6c1967a399e8cab6da2786adc40e
Izhharuddin Syanmil <email": null>
2016-10-18 16:22:59 +0700

    stuck on helpers; helper please help me

--- helpers/util.js
+++ helpers/util.js
@@ -1,10 +1,19 @@
 'use strict';
 
 var models = require('../models/index');
+var word = models.Words;
 var util = {};
 
 util.anagrams = function(source, callback){
-
+  console.log(source);
+  var hasil = []
+  word.findAll().then(function (data, err){
+    if (err){
+      return err
+    } else {
+      return data
+    }
+  })
 }
 
 module.exports = util;
