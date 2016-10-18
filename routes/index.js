d3ca8f6747d1680e3de7d0d0afb5d22ba94eee1f
Izhharuddin Syanmil <email": null>
2016-10-18 15:00:57 +0700

    form done

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var helper = require('../helpers/util');
 
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Anagrams'});
-});
-
-router.get('/:word', function(req, res, next) {
-  var word = req.params.word;
-  helper.anagrams(word, function(source, data){
-    res.render('index', { title: 'Anagrams', word: source, anagrams: data });
-  });
+  let kata = req.body.anagram
+  console.log(kata);
+  res.render('index', { title: 'Anagram-basic' });
 });
 
 module.exports = router;
