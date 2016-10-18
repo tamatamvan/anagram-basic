e37bd7ea849f19ce5572d417f8e960dee2a5feff
ahyanarizky <ahyana.rizky@gmail.com>
2016-10-18 19:39:03 +0700

    anagram edit

--- helpers/util.js
+++ helpers/util.js
@@ -1,21 +1,35 @@
 'use strict';
 
 var models = require('../models/index');
+var Sequelize = require('sequelize');
 var util = {};
 var word = models.Word
 var arr = []
 
 util.anagrams = function(source, callback){
   word.findAll({
-    attributes:['word']
+    // where: Sequelize.where(Sequelize.fn('char_length', Sequelize.col('word')), source.length)
   }).then(function (data) {
-    console.log(`success read`);
+    var temp = []
     var result = []
     for (var i = 0; i < data.length; i++) {
-      if (source.includes(data[i].dataValues.word)) {
-        result.push(data[i].dataValues.word)
+      temp.push(data[i].dataValues.word);
+    }
+
+    for (var i = 0; i < temp.length; i++) {
+      var sorted = source.split('').sort()
+      sorted = sorted.join()
+      sorted = sorted.replace(/,/g, "")
+      var tempo = temp[i].split('').sort()
+      tempo = tempo.join()
+      tempo = tempo.replace(/,/g, "")
+      console.log(sorted);
+      console.log(tempo);
+    if(sorted.indexOf(tempo) !== -1) {
+      result.push(temp[i])
       }
     }
+
     callback(source, result)
   })
 
